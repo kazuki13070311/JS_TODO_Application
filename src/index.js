9557fdd78c2f8b5880713523a499b3148dc47a46
kazuki13070311 <57397215+kazuki13070311@users.noreply.github.com>
2022-03-12 17:26:12 +0900

    test-add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する。
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li作成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ作成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // divタグの子要素に各要素を設定
+  li.appendChild(div).appendChild(p);
+
+  // 未完了のリストに追加
+  const addElement = document.getElementById("incomplete-list");
+  addElement.appendChild(li);
+};
+
+const addText = document.getElementById("add-button");
+addText.addEventListener("click", () => onClickAdd());
