df243f840bc8e16a2b78da765f0edd292d0d3144
kazuki13070311 <57397215+kazuki13070311@users.noreply.github.com>
2022-03-15 01:02:16 +0900

    削除機能まで実装

--- src/index.js
+++ src/index.js
@@ -16,8 +16,26 @@ const onClickAdd = () => {
   const p = document.createElement("p");
   p.innerText = inputText;
 
+  //button（完了）タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button（削除）タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグを未完了リストから削除
+    const deleteTarget = deleteButton.parentNode.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
   // divタグの子要素に各要素を設定
   li.appendChild(div).appendChild(p);
+  li.appendChild(div).appendChild(completeButton);
+  li.appendChild(div).appendChild(deleteButton);
 
   // 未完了のリストに追加
   const addElement = document.getElementById("incomplete-list");
